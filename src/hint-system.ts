cad2ebe4039fa162bca23c445741f70bcbd7e901
bauti777 <bauti777@gmail.com>
2023-12-02 03:04:59 -0300

    fix: some types? add: empty area checker

--- src/hint-system.ts
+++ src/hint-system.ts
@@ -1,8 +1,14 @@
 import { fixedRandomInt, fixedRandomNumber, readJsonFromFile } from './utils.js';
 import { Check } from './checks.js';
 
-// type LoreOverrides = Record<string, Record<string, [{ path: string; index: number }]>>
-export let loreMapReplacements: any
+interface LoreMap {
+    [mapId: string]: { path: string, index: number }
+}
+interface LoreOverrides { 
+    [mapName: string]: LoreMap 
+}
+
+export let loreMapReplacements: LoreOverrides
 
 // @ts-ignore
 const fs: typeof import('fs') = require('fs');
@@ -34,10 +40,7 @@ async function fetchHintList(): Promise<any> {
             return false
         }
     }
-    else {
-        return false
-    }
-    
+    return false
 }
 
 export async function generateHintList() {
@@ -83,16 +86,16 @@ export async function generateHint() { // outputs a random hint phrase from spoi
     let item
     let usefulItemList = getUsefulItemList(spoiler)    
 
-    let randomChance = fixedRandomNumber(getCurrentSeedWithOffset()) < 0.4 // Random chance to get a random item or just a useful item spoiler
+    let randomChance = randomSeededChance(0.4) // Random chance to get a random item or just a useful item spoiler
     randomChance 
         ? item = getRandomItem(usefulItemList)
         : item = getRandomItem(spoiler)
     
-
     // Extra checks for item type, in case its not a chest item, replaces map location info and key lock info
     let currentItemMap
     let currentItemLock 
     
+    // Throws a specific hint depending if item is on shop or element room
     switch (item.type) {
         case "shop":
             currentItemMap = "a certain shop"
@@ -142,55 +145,16 @@ function getRandomItem(spoiler: any) {
     return item
 }
 
-// Throws the area name based on the item's map
-function getArea(currentMap: any){
-    let areaInfo = currentMap.split(".");
-    areaInfo = areaInfo[0]
-
-    switch (areaInfo) { // workaround for weird area name cases
-        case "heat":
-            areaInfo = "heat-area"
-            break
-        case "autumn":
-            areaInfo = "autumn-area"
-            break
-        case "bergentrail":
-            areaInfo = "bergen-trails"
-            break
-        default:
-            break
-    }
-
-    if (sc.map.areas[areaInfo]) {
-        areaInfo = sc.map.areas[areaInfo].name["en_US"] // add a fix later on to match game language maybe
-    }
-    return areaInfo
-}
-
 async function getHintOutput(item: any, currentItemMap: string, currentItemLock: any, usefulItemList: any) {
-    let outputSpoiler
-
-    let usefulMapList = getUsefulItemMaps(usefulItemList)
-
-    // Generates random sentences
-    let randomOKSentence = getRandomPhrase(connectorStrings.usefulconnector)
-    let randomEmptySentence = [getRandomPhrase(connectorStrings.emptylocations[0]), getRandomPhrase(connectorStrings.emptylocations[1])]
-    let randomLockSentence = [getRandomPhrase(connectorStrings.lockedconnector[0]), getRandomPhrase(connectorStrings.lockedconnector[1])]
-    let randomKeySentence = [getRandomPhrase(connectorStrings.keylocations[0]), getRandomPhrase(connectorStrings.keylocations[1])]
-    
-    // Random chances to get a key lock, an area info, or a useless sentence
-    let doLockSentence = fixedRandomNumber(getCurrentSeedWithOffset()) < 0.4
-    let doUselessSentence = fixedRandomNumber(getCurrentSeedWithOffset()) < 0.05
-    let doAreaSentence = fixedRandomNumber(getCurrentSeedWithOffset()) < 0.74
-    let doMapSentence = fixedRandomNumber(getCurrentSeedWithOffset()) < 0.5
-
-    // Generates the entire hint phrase depending on the item, and sentences chances
-    let currentItemID = item.replacedWith.item
-
     let hintType
+    let outputSpoiler
+    let { randomEmptySentence, randomBadSentence, randomOKSentence, randomLockSentence, randomKeySentence } = generateSentences();
+    let { doLockSentence, doUselessSentence, doAreaSentence, doMapSentence } = generateSentenceChances();
 
-    let currentMap = await getMapName(item)
-
+    let usefulMapList = getUsefulItemMaps(usefulItemList);
+    // Generates the entire hint phrase depending on the item, and sentences chances
+    let currentMapName = await getMapName(item);
+    let currentItemID = item.replacedWith.item;
 
     if (doUselessSentence) { // lovely system to throw out a useful hint entirely
         outputSpoiler = getRandomPhrase(connectorStrings.uselessphrases)
@@ -209,48 +173,50 @@ async function getHintOutput(item: any, currentItemMap: string, currentItemLock:
                 hintType = "Item Hint"
                 if (doAreaSentence) {
                     hintType = "Location Hint"
-                    currentItemMap = getArea(currentItemMap)
+                    currentMapName = getArea(currentItemMap)
                 }
-                outputSpoiler = (getItemInfo(currentItemID) + " " +  randomOKSentence + " " + currentMap)
+                outputSpoiler = (getItemInfo(currentItemID) + " " +  randomOKSentence + " " + currentMapName)
             }
         }
+
         // throw a sentence that has info on a certain map
         else if (doMapSentence && (item.type == "chest" || item.type == "event")) {
             hintType = "Location Hint"
             if (usefulMapList.includes(currentItemMap)) {
-                outputSpoiler = (currentMap + " " + randomKeySentence[0])
+                outputSpoiler = (currentMapName + " " + randomKeySentence[0])
             }
             else {
-                outputSpoiler = (currentMap + " " + randomKeySentence[1])
+                outputSpoiler = (currentMapName + " " + randomKeySentence[1])
             }
         }
+
         // if the check is not a useful item just throw a empty location sentence
         else { 
-                outputSpoiler = (randomEmptySentence[0] + currentMap + " " +  randomEmptySentence[1]) 
+            if (checkMapEmpty(currentItemMap) && randomSeededChance(0.3)) { // check if completely empty location
+                if (doAreaSentence) {
+                    hintType = "Location Hint"
+                    currentMapName = getArea(currentItemMap)
+                }
+                outputSpoiler = (currentMapName + " " +  randomEmptySentence) 
                 hintType = "Location Hint"
+            }
+            else { // otherwise sends a vague info
+                outputSpoiler = (randomBadSentence[0] + currentMapName + " " +  randomBadSentence[1]) 
+                hintType = "Location Hint"
+            }
         }
     }
 
     return [outputSpoiler, hintType]
 }
 
-// gets the item's name
-function getItemInfo(item: any) {
-    return ig.vars.get("item." + item + ".name")
-}
-
-
-// grabs the item ID and matches it with the useful_items array
-const USEFUL_ITEMS = [145,149,170,154,155,156,225,153,236,376,147,230,345,286,231,410,439,434,350]
-// const USEFUL_ITEMS_KEYS = [152,146,272,319,349]
-function isUsefulItem(currentItem: any) { 
-    let isUseful
-    for (let i = 0; i < USEFUL_ITEMS.length; i++ ){
-        if (currentItem == USEFUL_ITEMS[i]) {
-            return isUseful = true
+function checkMapEmpty(map: any) {
+    for (let check of spoilerLog) {
+        if (check.map == map) {
+            if (isUsefulItem(check.replacedWith.item)) { return false; }
         }
     }
-    return isUseful = false
+    return true
 }
 
 // just slaps a random phrase from the string dict
@@ -261,46 +227,25 @@ function getRandomPhrase (obj: any) {
 var connectorStrings = { // a dictionary for phrase variance
     usefulconnector: ["is located in", "can be found in", "is somewhere in", "is hidden behind"],
     lockedconnector: [["is locked by a", "is hidden behind a"], ["key", "lock", "chest"]],
-    emptylocations: [["A pickup in ", "A check in ", "One of the chests in ", "Something under "],
+    badlocations: [["A pickup in ", "A check in ", "One of the chests in ", "Something under "],
     ["has no important items", "might not have anything important", "is not a key item", "might have nothing", "is not relevant"]],
+    emptylocations: ["has no important items", "is barren", "has no key items", "is not relevant"],
     uselessphrases: ["Thanks for playing rando", "A hint was here but someone stole it", "Insert awful phrases to fill hint space", "Did you know worms have five hearts? Just like real earth worms do", "Sponsored by Project Terra unwalkable fences", "Year of the Crosscode", "JADC stands for Jump Attack Dash Cancel", "There is Just Water in Bergen", "Star Shade is located in Master Sword pedestal"],
-    keylocations: [["has an important item", "has a key item", "holds something useful", "yields a key item", "might be worth visiting", "might have what you're looking for"],["has no key items", "is dry on pickups","is an empty location", "might be empty", "is completely void of key items","might not have what you're looking for"]]
-}
-
-// A cheap function to get the same hint values on a specific seed
-function getCurrentSeedWithOffset() {
-    let seedOffset = currentSeed + seedIndex
-    seedIndex++
-    return seedOffset
+    keylocations: [["has an important item", "has a key item", "holds something useful", "yields a key item", "might be worth visiting", "might have what you're looking for"],["has no key items", "is dry on pickups","is an empty location", "might be empty", "is completely void of key items","might not have what you're looking for"]],
+    importantlocations: { element: "has an element", key: "has a key item", shade: "has a shade" }
 }
 
 
 // Returns hint info from a specific entry on the list, to inject in the game
-export function getHintListEntry(check: any, map: any) {
-    let index: number = getHintIndex(check, map)
+export function getHintListEntry(mapCheckId: string, map: string) {
+    let index: number = getHintIndex(mapCheckId, map)
     if (hintList && hintList[index]) {
         return {
             "icon": "INFO",
-            "hoverText": {
-              "en_US": hintList[index].type,
-              "de_DE": hintList[index].type,
-              "zh_CN": hintList[index].type,
-              "ja_JP": hintList[index].type,
-              "ko_KR": hintList[index].type,
-              "langUid": 886,
-              "zh_TW": hintList[index].type
-            },
+            "hoverText": { "en_US": hintList[index].type, "de_DE": hintList[index].type, "zh_CN": hintList[index].type, "ja_JP": hintList[index].type, "ko_KR": hintList[index].type, "langUid": 886, "zh_TW": hintList[index].type },
             "event": [
               {
-                  "text": {
-                  "en_US": hintList[index].hint,
-                  "de_DE": hintList[index].hint,
-                  "zh_CN": hintList[index].hint,
-                  "ja_JP": hintList[index].hint,
-                  "ko_KR": hintList[index].hint,
-                  "langUid": 887,
-                  "zh_TW": hintList[index].hint
-                },
+                  "text": { "en_US": hintList[index].hint, "de_DE": hintList[index].hint, "zh_CN": hintList[index].hint, "ja_JP": hintList[index].hint, "ko_KR": hintList[index].hint, "langUid": 887, "zh_TW": hintList[index].hint },
                 "center": false,
                 "autoContinue": false,
                 "type": "SHOW_BOARD_MSG"
@@ -311,36 +256,13 @@ export function getHintListEntry(check: any, map: any) {
     else {
         return {
             "icon": "INFO",
-            "hoverText": {
-              "en_US": "Missing Hint",
-              "de_DE": "Missing Hint",
-              "zh_CN": "Missing Hint",
-              "ja_JP": "Missing Hint",
-              "ko_KR": "Missing Hint",
-              "langUid": 886,
-              "zh_TW": "Missing Hint"
-            },
-            "event": [
-              {
-                  "text": {
-                  "en_US": "Missing Hint",
-                  "de_DE": "Missing Hint",
-                  "zh_CN": "Missing Hint",
-                  "ja_JP": "Missing Hint",
-                  "ko_KR": "Missing Hint",
-                  "langUid": 887,
-                  "zh_TW": "Missing Hint"
-                },
-                "center": false,
-                "autoContinue": false,
-                "type": "SHOW_BOARD_MSG"
-              }
-            ]
+            "hoverText": { "en_US": "Missing Hint", "de_DE": "Missing Hint", "zh_CN": "Missing Hint", "ja_JP": "Missing Hint", "ko_KR": "Missing Hint", "langUid": 886, "zh_TW": "Missing Hint" },
+            "event": [ { "text": { "en_US": "Missing Hint", "de_DE": "Missing Hint", "zh_CN": "Missing Hint", "ja_JP": "Missing Hint", "ko_KR": "Missing Hint", "langUid": 887, "zh_TW": "Missing Hint" }, "center": false, "autoContinue": false, "type": "SHOW_BOARD_MSG" } ]
         }  
     }
 }
 
-function getHintIndex(check: any, map: any) {
+function getHintIndex(mapId: string, map: string) {
     let index = 0  
     let loreOverride = loreMapReplacements[map]
     let replacements = Object.keys(loreMapReplacements)
@@ -348,7 +270,7 @@ function getHintIndex(check: any, map: any) {
     for (let i = 0; i < replacements.length; i++) {
         if (map == replacements[i]) {
             for (let subIndex = 0; subIndex < Object.keys(loreOverride).length; subIndex++) {
-                if (check == Object.keys(loreOverride)[subIndex])
+                if (mapId == Object.keys(loreOverride)[subIndex])
                 {
                     return index 
                 }
@@ -382,22 +304,94 @@ export async function setLoreOverrides(directory: any) {
     loreMapReplacements = await readJsonFromFile(directory + "data/lore-replacements.json");
 }
 
+// Throws the area name based on the item's map
+async function getArea(currentMap: any){
+    let areaInfo = currentMap.split(".");
+    areaInfo = areaInfo[0]
+
+    switch (areaInfo) { // workaround for weird area name cases
+        case "heat":
+            areaInfo = "heat-area"
+            break
+        case "autumn":
+            areaInfo = "autumn-area"
+            break
+        case "bergentrail":
+            areaInfo = "bergen-trails"
+            break
+        default:
+            break
+    }
+
+    if (sc.map.areas[areaInfo]) {
+        areaInfo = await sc.map.areas[areaInfo].name["en_US"] // add a fix later on to match game language maybe
+    }
+    return areaInfo
+}
 
 // find a way to refer to generateRandomizerState.getPrettyName and just do everything with that function instead
 async function getMapName(log: Check) {
     const database = await readJsonFromFile('assets/data/database.json');
     const shopsDatabase = database.shops;
 
-    if (log.type === 'shop') {
-        return shopsDatabase[log.name].name.en_US;
-    }
-
-    if ('name' in log) {
-        return log.name; //TODO: quest names
-    }
+    if (log.type === 'shop') { return shopsDatabase[log.name].name.en_US; };
+    //TODO: quest names 
+    if ('name' in log) { return log.name; };
 
     let fullMapName = log.mapName.split(/\-/g);
     let mapName = fullMapName[1].slice(1)
     
     return mapName;
 }
+
+function generateSentences() {
+    let randomOKSentence = getRandomPhrase(connectorStrings.usefulconnector)
+    let randomBadSentence = [getRandomPhrase(connectorStrings.badlocations[0]), getRandomPhrase(connectorStrings.badlocations[1])]
+    let randomEmptySentence = getRandomPhrase(connectorStrings.emptylocations)
+    let randomLockSentence = [getRandomPhrase(connectorStrings.lockedconnector[0]), getRandomPhrase(connectorStrings.lockedconnector[1])]
+    let randomKeySentence = [getRandomPhrase(connectorStrings.keylocations[0]), getRandomPhrase(connectorStrings.keylocations[1])]
+
+    return { randomEmptySentence, randomOKSentence, randomLockSentence, randomKeySentence, randomBadSentence };
+}
+
+function generateSentenceChances() {
+        let doLockSentence = randomSeededChance(0.4)
+        let doUselessSentence = randomSeededChance(0.05)
+        let doAreaSentence = randomSeededChance(0.74)
+        let doMapSentence = randomSeededChance(0.5)
+
+        return { doLockSentence, doUselessSentence, doAreaSentence, doMapSentence };
+}
+
+// utils
+function randomSeededChance(chance: number) {
+    return (fixedRandomNumber(getCurrentSeedWithOffset()) < chance);
+}
+
+function getCurrentSeedWithOffset() {
+    let seedOffset = currentSeed + seedIndex
+    seedIndex++
+    return seedOffset
+}
+
+const USEFUL_ITEMS = [145,149,170,154,155,156,225,153,236,376,147,230,345,286,231,410,439,434,350,"heat","shock","wave","cold"]
+const ELEMENTS = ["heat", "shock", "cold", "wave"]
+function isUsefulItem(currentItem: any) { 
+    if (USEFUL_ITEMS.includes(currentItem)) {
+        return true;
+    }
+    return false;
+}
+
+function isElement(currentItem: any) { 
+    if (ELEMENTS.find(currentItem)) {
+        return true;
+    }
+    return false;
+}
+
+// gets the item's name
+function getItemInfo(item: any) {
+    return ig.vars.get("item." + item + ".name")
+}
+
